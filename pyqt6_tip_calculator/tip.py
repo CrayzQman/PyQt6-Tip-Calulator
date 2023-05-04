817e10f01314a7af0ed9a465c6fef3982c4f1070
CrayzQman <hughq243@hsd.k12.or.us>
2023-05-04 12:46:27 -0700

    Adding layout

--- pyqt6_tip_calculator/tip.py
+++ pyqt6_tip_calculator/tip.py
@@ -1,22 +1,40 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton,
+                             QLineEdit, QLabel, QWidget, QVBoxLayout)
 
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(QSize(500, 400))
+        self.setWindowTitle("Tip Calculator")
+        layout = QVBoxLayout()
 
-        self.setWindowTitle("My App")
+        button = QPushButton("calculate")
 
-        button = QPushButton("Press Me!")
+        text = QLineEdit()
+        text.setMaxLength(900)
+        text.setPlaceholderText("Enter your text")
 
-        self.setFixedSize(QSize(800, 700))
+        title = QLabel("Tip Calculator")
+        font = title.font()
+        font.setPointSize(30)
+        title.setFont(font)
+        title.setAlignment(Qt.AlignmentFlag.AlignHCenter | 
+                           Qt.AlignmentFlag.AlignTop)
+
+        # Add widgets to the layout
+        layout.addWidget(title)
+        layout.addWidget(text)
+        layout.addWidget(button)
 
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
